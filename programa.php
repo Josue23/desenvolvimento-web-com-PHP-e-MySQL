e6b2615db069561b0126ad47690eb1846a9a0f7a
josue23 <josuerodrigues@gmail.com>
2016-07-06 23:58:24 -0300

    inseri comentários

--- programa.php
+++ programa.php
@@ -1,4 +1,9 @@
 <?php
+// exibe o weekday e dia/mês/ano
 echo "Hoje é " . date(l) . " dia " . date('d/m/y') . '<br />';
+
+// exibe hora no formato AM PM
 echo " agora são " . date('h:i:s A') . '<br />';
+
+// exemplo: hoje é quarta feira e faltam 3 dias para sábado
 echo "Hoje é " . date(l) . ", faltam três dias para " . date('l', strtotime("+3 days")) . '<br />';
